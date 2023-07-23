159ea7b425f36287c7f6869f256510218f7726fa
nathaniel-mercado <nmercado0517@gmail.com>
2023-07-23 19:00:31 +0800

    Updated Links for each subject

--- vite-project/src/sub_details.jsx
+++ vite-project/src/sub_details.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 function web_details(){
     let webs = [
-        {"name":"Coding", "description":"Resources for beginning your coding journey","picture":"https://upload.wikimedia.org/wikipedia/commons/3/3d/Code-1076536.jpg","links":[{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/math"},{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/math"}]},
-        {"name":"Math", "description":"Resources for beginning your math journey", "picture":"https://i0.wp.com/calmatters.org/wp-content/uploads/2021/08/math-curriculum.jpg?fit=1200%2C900&ssl=1","links":[{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/math"}]},
-        {"name":"Science", "description":"Resources for beginning your science journey", "picture":"https://www.bibalex.org/SCIplanet/Attachments/Theme/MediumImage/qufOnGG7Ox_20210929150223214.jpg","links":[{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/math"}]},
+        {"name":"Coding", "description":"Resources for beginning your coding journey","picture":"https://upload.wikimedia.org/wikipedia/commons/3/3d/Code-1076536.jpg","links":[{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/math"},{"site": "W3Schools", "hyperlink": "https://www.w3schools.com/"},{"site":"Harvard's CS50","hyperlink":"https://cs50.harvard.edu/x/2023/"}]},
+        {"name":"Math", "description":"Resources for beginning your math journey", "picture":"https://i0.wp.com/calmatters.org/wp-content/uploads/2021/08/math-curriculum.jpg?fit=1200%2C900&ssl=1","links":[{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/math"},{"site": "The Organic Chemistry Tutor", "hyperlink": "https://www.youtube.com/@TheOrganicChemistryTutor"}]},
+        {"name":"Science", "description":"Resources for beginning your science journey", "picture":"https://www.bibalex.org/SCIplanet/Attachments/Theme/MediumImage/qufOnGG7Ox_20210929150223214.jpg","links":[{"site": "Khan Academy", "hyperlink": "https://www.khanacademy.org/science"},{"site":"Crash Course","hyperlink":"https://thecrashcourse.com/"}]},
         
     
     ]
