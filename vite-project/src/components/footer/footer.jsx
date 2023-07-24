d3af777a09ed020662d6707421a930a1e1d2eff2
nathaniel-mercado <nmercado0517@gmail.com>
2023-07-24 23:53:06 +0800

    Linked GitHub Repository to Footer

--- vite-project/src/components/footer/footer.jsx
+++ vite-project/src/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 import "./footer.css"
 const Footer = () => {
     return <div class="footer">
-        <p>The links and photos contained in this website do not belong to me, proper attribution can be found in the corresponding GitHub Repository.</p>
+        <p>The links and photos contained in this website do not belong to me, proper attribution can be found in the corresponding <a href="https://github.com/Mr-Neytan/EduLink" target="_blankqq">GitHub Repository</a>.</p>
     </div>
 }
 
